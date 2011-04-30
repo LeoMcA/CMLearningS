4e2452cc324165c7a5c4b2646743c3015c48c373
leomca <leomcardle@gmail.com>
2011-04-30 22:25:33 +0000

    added redirections to add.php

--- add.php
+++ add.php
@@ -2,4 +2,5 @@
 $file = fopen($_POST["subject"] . "-" . $_POST["topic"] . "-" . $_POST["subtopic"] . ".xml","w");
 echo fwrite($file,"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><root><subject>" . $_POST["subject"] . "</subject><topic>" . $_POST["topic"] . "</topic><subtopic>" . $_POST["subtopic"] . "</subtopic><notes>" . $_POST["notes"] . "</notes></root>");
 fclose($file);
+header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]);
 ?>
