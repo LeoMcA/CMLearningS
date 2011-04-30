b805d07bc445ac3cb93e4cda5e5eccdfa1755b37
leomca <leomcardle@gmail.com>
2011-04-30 19:56:11 +0000

    added missing semi-colon in index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if($_GET["subject"]==NULL) $page = "index";
 elseif($_GET["topic"]==NULL) $page = "topic-index";
 elseif($_GET["subtopic"]==NULL) $page = "subtopic-index";
-else $page = "notes"
+else $page = "notes";
 ?>
 <!doctype html>
 <html>
