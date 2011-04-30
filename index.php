05300ec7b04314c1dc71e4f9bd69f3b87c90825b
leomca <leomcardle@gmail.com>
2011-04-30 21:40:46 +0000

    added writing of all the subjects in the index

--- index.php
+++ index.php
@@ -3,13 +3,19 @@ if($_GET["subject"]==NULL) $page = "index";
 elseif($_GET["topic"]==NULL) $page = "topic-index";
 elseif($_GET["subtopic"]==NULL) $page = "subtopic-index";
 else $page = "notes";
-?>
 <!doctype html>
 <html>
 <head>
     <title><?php if($page=="index") echo "Subject Index"; elseif($page=="topic-index") echo "Topic Index for " . $_GET["subject"]; elseif($page=="subtopic-index") echo "Subtopic Index for " . $_GET["topic"]; elseif($page=="notes") echo "Notes for " . $_GET["subtopic"]; ?></title>
 </head>
 <body>
-<h1><?php if($page=="index") echo "Subject Index"; elseif($page=="topic-index") echo "Topic Index for " . $_GET["subject"]; elseif($page=="subtopic-index") echo "Subtopic Index for " . $_GET["topic"]; elseif($page=="notes") echo "Notes for " . $_GET["subtopic"]; ?></h1>
+    <h1><?php if($page=="index") echo "Subject Index"; elseif($page=="topic-index") echo "Topic Index for " . $_GET["subject"]; elseif($page=="subtopic-index") echo "Subtopic Index for " . $_GET["topic"]; elseif($page=="notes") echo "Notes for " . $_GET["subtopic"]; ?></h1>
+    <?php
+    $filenames = glob("*-*-*.xml");
+    if($page=="index")
+        foreach($filnames as $value) {
+            $filename = explode("-",$value);
+            echo $filename[0];
+        }
 </body>
 </html>
