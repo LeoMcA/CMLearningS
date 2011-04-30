70cbac684d8ab9fe7758ca205c4f8ed36abbbd21
leomca <leomcardle@gmail.com>
2011-04-30 19:55:33 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+if($_GET["subject"]==NULL) $page = "index";
+elseif($_GET["topic"]==NULL) $page = "topic-index";
+elseif($_GET["subtopic"]==NULL) $page = "subtopic-index";
+else $page = "notes"
+?>
+<!doctype html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+</body>
+</html>
