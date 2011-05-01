3133174ed196a2002d1b4d38560de3c2c003c865
leomca <leomcardle@gmail.com>
2011-05-01 15:32:54 +0000

    fixed error whilst using sqlite_query()

--- add.php
+++ add.php
@@ -10,11 +10,11 @@ else {
     $query = 'CREATE TABLE Table' .
          '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT)';
     
-    sqlite_query($query);
+    sqlite_query($database, $query);
 }
 
 //insert data into database
 $query = 'INSERT INTO Table (Subject, Topic, Subtopic, Notes) VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");';
-    sqlite_query($query);
+    sqlite_query($database, $query);
 ?>
 <?php header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]); ?>
