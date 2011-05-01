d89d47ec4f2979e73d963fa9ca3ccb946a15f853
leomca <leomcardle@gmail.com>
2011-05-01 14:37:13 +0000

    attempt to fix a syntax error on line 30

--- add.php
+++ add.php
@@ -25,7 +25,6 @@ else {
 //insert data into database
 $query =
     'INSERT INTO Table (Subject, Topic, Subtopic, Notes) ' .
-    'VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '"); '
-
-header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]);
+    'VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");'
 ?>
+<?php header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]); ?>
