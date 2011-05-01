a9ea5b3a23d38b5c01ea1f459f47e8e1de5a102b
leomca <leomcardle@gmail.com>
2011-05-01 15:23:58 +0000

    wasn't properly querying the db, now fixed

--- add.php
+++ add.php
@@ -9,11 +9,14 @@ else {
     
     $query = 'CREATE TABLE Table' .
          '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT)';
+    
+    sqlite_query($query);
 }
 
 //insert data into database
 $query =
     'INSERT INTO Table (Subject, Topic, Subtopic, Notes) ' .
     'VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");'
+    sqlite_query($query);
 ?>
 <?php header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]); ?>
