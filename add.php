34bbba7db23dd96ad21720fa141dc4287bff754f
leomca <leomcardle@gmail.com>
2011-05-01 14:34:46 +0000

    removed two ifs that were causing errors

--- add.php
+++ add.php
@@ -20,20 +20,12 @@ else {
     
     $query = 'CREATE TABLE Table' .
          '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT)';
-         
-    if(!$database->queryExec($query, $error)) {
-        die($error);
-    }
 }
 
 //insert data into database
 $query =
     'INSERT INTO Table (Subject, Topic, Subtopic, Notes) ' .
     'VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '"); '
-
-if(!$database->queryExec($query, $error)) {
-    die($error);
-}
 
 header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]);
 ?>
