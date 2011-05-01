3af5c91ec1b3075675296a6828c2f8cb09c44615
leomca <leomcardle@gmail.com>
2011-05-01 00:18:37 +0000

    attempt to fix naming of subtopics

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ else $page = "notes";
         echo "</ul>";
     }
     if($page=="notes"){
-        $xml = simplexml_load_file($_GET["subject"] . "-" . $_GET["topic"] . "-" . $_GET["subtopic"] ".xml");
+        $xml = simplexml_load_file($_GET["subject"] . "-" . $_GET["topic"] . "-" . $_GET["subtopic"] . ".xml");
         echo $xml->root->notes;
     }
     ?>
