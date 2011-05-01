ed3413cc0323da4f8c2783f03f5e88c1b3af4a1a
leomca <leomcardle@gmail.com>
2011-05-01 15:35:35 +0000

    changed name of table to revision, to see if it fixes anything

--- add.php
+++ add.php
@@ -7,14 +7,14 @@ else {
     
     $database = sqlite_open('db.sqlite', 0666, $error);
     
-    $query = 'CREATE TABLE Table' .
+    $query = 'CREATE TABLE Revision' .
          '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT)';
     
     sqlite_query($database, $query);
 }
 
 //insert data into database
-$query = 'INSERT INTO Table (Subject, Topic, Subtopic, Notes) VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");';
+$query = 'INSERT INTO Revision (Subject, Topic, Subtopic, Notes) VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");';
     sqlite_query($database, $query);
 ?>
 <?php header( 'Location: index.php?subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]); ?>
