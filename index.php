7f0b68364f12e4f0f1871b44db64fb07dfef6146
leomca <leomcardle@gmail.com>
2011-05-01 00:02:54 +0000

    added support for notes pages

--- index.php
+++ index.php
@@ -52,6 +52,10 @@ else $page = "notes";
         }
         echo "</ul>";
     }
+    if($page=="notes"){
+        $xml = simplexml_load_file($_GET["subject"] . "-" . $_GET["topic"] . "-" . $_GET["subtopic"] . ".xml");
+        echo $xml->notes;
+    }
     ?>
 </body>
 </html>
