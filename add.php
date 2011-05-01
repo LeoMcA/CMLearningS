fc2902c27f19ac124d040fcfa28cd2382db81570
leomca <leomcardle@gmail.com>
2011-05-01 23:06:11 +0000

    attempted to fix ga-ga db

--- add.php
+++ add.php
@@ -5,7 +5,6 @@ $query = 'CREATE TABLE Revision' .
          '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT)';
     
 sqlite_query($database, $query);
-}
 
 //insert data into database
 $query = 'INSERT INTO Revision (Subject, Topic, Subtopic, Notes) VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");';
