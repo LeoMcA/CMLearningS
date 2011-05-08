828c9ff6ef01828952d54fc00fb343004a0e3921
leomca <leomcardle@gmail.com>
2011-05-08 21:23:19 +0000

    converted the whole of db to oophp

--- class.php
+++ class.php
@@ -5,9 +5,9 @@ class page {
     
     function get_page() {
         if($_GET["subject"]==NULL) $this->page = "index";
-            elseif($_GET["topic"]==NULL) $this->page = "topic-index";
-                elseif($_GET["subtopic"]==NULL) $this->page = "subtopic-index";
-                    else $this->page = "notes";
+        if($_GET["topic"]==NULL) $this->page = "topic-index";
+        if($_GET["subtopic"]==NULL) $this->page = "subtopic-index";
+            else $this->page = "notes";
     }
     
     function return_page() {
@@ -26,6 +26,28 @@ class database {
         sqlite_query($database, $query);
         $query = 'INSERT INTO Revision (Subject, Topic, Subtopic, Notes) VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", "' . $_POST["notes"] . '");';
         sqlite_query($database, $query);
+    }
+    
+    function get_database() {
+        $page = new page();
+        $query = "SELECT Subject, Topic, Subtopic, Notes FROM Revision";
+        $result = sqlite_array_query(sqlite_open('db.sqlite', 0666, $error), $query);
+        $item = array();
+        foreach($result as $value) {
+            if($page->return_page()=="index") $item[] = $value[Subject];
+            if($page->return_page()=="topic-index") if($value[Subject]==$_GET["subject"]) $item[] = $value[Topic];
+            if($page->return_page()=="subtopic-index") if($value[Subject]==$_GET["subject"]) if($value[Topic]==$_GET["topic"]) $item[] = $value[Subtopic];
+            if($page->return_page()=="notes") if($value[Subject]==$_GET["subject"]) if($value[Topic]==$_GET["topic"]) if($value[Subtopic]==$_GET["subtopic"]) $item[] = $value[Notes];
+        }
+        $item = array_unique($item);
+        if($page->return_page()=="notes") echo $item[0]; else {
+            echo "<ul>";
+            foreach($item as $value) {
+                echo "<li><a href=\"index.php?subject="; if($page->return_page()=="topic-index") echo $_GET["subject"]."&topic="; if($page->return_page()=="subtopic-index") echo $_GET["topic"]."&subtopic="; echo $value."\">".$value."</a></li>";
+            }
+            echo "</ul>";
+        }
+        
     }
     
     function accessdb() {
