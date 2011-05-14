40dab073ea0ddd85277e008c9a69b739849c4527
leomca <leomcardle@gmail.com>
2011-05-14 17:34:20 +0000

    added class.php and the detection of page via oophp

---
+++ class.php
@@ -0,0 +1,25 @@
+<?php
+    class page {
+        
+        var $page;
+        
+        function set_page() {
+            if($_GET["subject"]==NULL) $this->page = "index";
+            elseif($_GET["topic"]==NULL) $this->page = "topic-index";
+            elseif($_GET["subtopic"]==NULL) $this->page = "subtopic-index";
+            else $this->page = "notes";
+        }
+        
+        function if_page($i,$ti,$sti,$n) {
+            if($this->page = "index") $i;
+            if($this->page = "topic-index") $ti;
+            if($this->page = "subtopic-index") $sti;
+            if($this->page = "notes") $n;
+        }
+        
+        function get_page() {
+            return $this->page;
+        }
+    
+    }
+?>
