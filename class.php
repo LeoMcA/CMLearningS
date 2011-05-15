f415ec42a87a297c7652d09b362d3c06b378d449
leomca <leomcardle@gmail.com>
2011-05-15 11:31:46 +0000

    copied accessdb function to class.php

--- class.php
+++ class.php
@@ -29,4 +29,51 @@
         }
     
     }
+    
+    function accessdb() {
+    global $database;
+    global $page;
+    $query = "SELECT Subject, Topic, Subtopic, Notes FROM Revision";
+    $result = $database->arrayQuery($query);
+    $item = array();
+    foreach($result as $value) {
+        if($page->get_page()=="index") {
+            $item[] = $value[Subject];
+        }
+        if($page->get_page()=="topic-index") {
+            if($value[Subject]==$_GET["subject"]) {
+                $item[] = $value[Topic];
+            }
+        }
+        if($page->get_page()=="subtopic-index") {
+            if($value[Subject]==$_GET["subject"]) {
+                if($value[Topic]==$_GET["topic"]) {
+                    $item[] = $value[Subtopic];
+                }
+            }
+        }
+        if($page->get_page()=="notes") {
+            if($value[Subject]==$_GET["subject"]) {
+                if($value[Topic]==$_GET["topic"]) {
+                    if($value[Subtopic]==$_GET["subtopic"]) {
+                        $item[] = $value[Notes];
+                    }
+                }
+            }
+        }
+    }
+    $item = array_unique($item);
+    if($page->get_page()=="notes") {
+        echo $item[0];
+    }
+    else {
+        echo "<ul>";
+        foreach($item as $value) {
+            echo "<li><a href=\"index.php?subject=";
+            echo $page->if_page('',$_GET["subject"]."&topic=",$_GET["subject"]."&topic=".$_GET["topic"]."&subtopic="); 
+            echo $value."\">".$value."</a></li>";
+        }
+        echo "</ul>";
+    }
+    }
 ?>
--- index.php
+++ index.php
@@ -6,53 +6,6 @@
     $page = new page;
     
     $database = new SQLiteDatabase('db.sqlite', 0666);
-    
-    function accessdb() {
-    global $database;
-    global $page;
-    $query = "SELECT Subject, Topic, Subtopic, Notes FROM Revision";
-    $result = $database->arrayQuery($query);
-    $item = array();
-    foreach($result as $value) {
-        if($page->get_page()=="index") {
-            $item[] = $value[Subject];
-        }
-        if($page->get_page()=="topic-index") {
-            if($value[Subject]==$_GET["subject"]) {
-                $item[] = $value[Topic];
-            }
-        }
-        if($page->get_page()=="subtopic-index") {
-            if($value[Subject]==$_GET["subject"]) {
-                if($value[Topic]==$_GET["topic"]) {
-                    $item[] = $value[Subtopic];
-                }
-            }
-        }
-        if($page->get_page()=="notes") {
-            if($value[Subject]==$_GET["subject"]) {
-                if($value[Topic]==$_GET["topic"]) {
-                    if($value[Subtopic]==$_GET["subtopic"]) {
-                        $item[] = $value[Notes];
-                    }
-                }
-            }
-        }
-    }
-    $item = array_unique($item);
-    if($page->get_page()=="notes") {
-        echo $item[0];
-    }
-    else {
-        echo "<ul>";
-        foreach($item as $value) {
-            echo "<li><a href=\"index.php?subject=";
-            echo $page->if_page('',$_GET["subject"]."&topic=",$_GET["subject"]."&topic=".$_GET["topic"]."&subtopic="); 
-            echo $value."\">".$value."</a></li>";
-        }
-        echo "</ul>";
-    }
-}
 ?>
 <!doctype html>
 <html>
@@ -60,8 +13,6 @@
 </head>
 <body>
     <title style="display:block;"><?php echo $page->if_page("Subject Index","Topic Index for ".$_GET["subject"],"Subtopic Index for ".$_GET["topic"],"Notes for ".$_GET["subtopic"]); ?></title>
-    <?php
-    accessdb();
-    ?>
+    <?php accessdb(); ?>
 </body>
 </html>
