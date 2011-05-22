ddc0852cd6b5fb83b46fa091eea8b5fee0751b8e
leomca <leomcardle@gmail.com>
2011-05-22 13:25:23 +0000

    added new login/add page with php session

---
+++ ad.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <title><?php if(isset($_SESSION['user'])){ echo "Add page"; } else{ echo "Please login"; } ?></title>
+</head>
+<body>
+<?php
+    if(isset($_SESSION['user'])) {
+        echo "<form action='add.php' method='post'>" .
+            "Subject: <br><input type='text' name='subject'><br>" .
+            "Topic: <br><input type='text' name='topic'><br>" .
+            "Subtopic: <br><input type='text' name='subtopic'><br>" .
+            "Notes: <br><textarea rows='2' cols='20' name='notes'></textarea><br>" .
+            "<input type='submit' value='Submit'>" .
+            "</form>";
+    }
+    else {
+        echo "<form action='login.php' method='post'>" . 
+        "Username: <br><input type='text' name='username'><br>" .
+        "Password: <br><input type='text' name='password'><br>" .
+        "<input type='submit' value='Submit'>" .
+        "</form>";
+</body>
+</html>
