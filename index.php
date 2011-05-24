9de703b872c831cc1ad853abf786214f8ac4b231
leomca <leomcardle@gmail.com>
2011-05-24 19:56:53 +0000

    continued putting everything in index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <title></title>
 </head>
 <body>
-    <a href='logout.php'>Logout</a>
+    <a href='index.php?page=logout'>Logout</a>
     <h1></h1>
     <?php
         if($page->get_page()=='i') {
@@ -45,15 +45,43 @@
         if($page->get_page()=='n') {
             $page->print_notes($page->get_notes());
         }
+        
+        if($page->get_page()=='li') {
+            if($user->get_user()=='false') {
+                printf('Sorry, that user does not exsist');
+            }
+            else {
+                $username = $user->get_user();
+                $_SESSION['user'] = $username[0];
+                printf('Logged in');
+            }
+        }
+        
+        if($page->get_page()=='lo') {
+            session_destroy();
+            printf('Loggged out');
+        }
+        
+        if($page->get_page()=='a') {
+            $query = 'CREATE TABLE Revision' .
+                '(Subject TEXT, Topic TEXT, Subtopic TEXT, Notes TEXT);';
+            $database->queryExec($query);
+
+            $query = 'INSERT INTO Revision (Subject, Topic, Subtopic, Notes)' .
+                'VALUES ("' . $_POST["subject"] . '", "' . $_POST["topic"] . '", "' . $_POST["subtopic"] . '", \'' . $_POST["notes"] . '\');';
+            $database->queryExec($query);
+
+            header( 'Location: index.php?page=notes&subject=' .  $_POST["subject"] . '&topic=' . $_POST["topic"] . '&subtopic=' . $_POST["subtopic"]);
+        }
     ?>
-    <form action='add.php' method='post'>
+    <form action='index.php?page=add' method='post'>
     Subject: <br><input type='text' name='subject'><br>
     Topic: <br><input type='text' name='topic'><br>
     Subtopic: <br><input type='text' name='subtopic'><br>
     Notes: <br><textarea style='width:100%;height:500px;' name='notes'></textarea><br>
     <input type='submit' value='Submit'>
     </form>
-    <form action='login.php' method='post'>
+    <form action='index.php?page=login' method='post'>
     Username: <br><input type='text' name='username'><br>
     Password: <br><input type='password' name='password'><br>
     <input type='submit' value='Submit'>
